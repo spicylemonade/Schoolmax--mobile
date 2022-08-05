637d5547ce66c96671d076b316fb315b7ce88318
spicylemonade <gebytienyi@gmail.com>
2022-08-05 15:09:49 -0400

    home config

--- src/screens/mainStack/Home.js
+++ src/screens/mainStack/Home.js
@@ -1,13 +1,43 @@
 import React from "react";
 import { enableScreens } from "react-native-screens";
 import { StyleSheet, Text, View } from "react-native";
-
+import { useFonts, Poppins_600SemiBold } from "@expo-google-fonts/poppins";
+//import Icon from "@expo/vector-icons/MaterialIcons";
+import AppLoading from "expo-app-loading";
 const Home = () => {
+  let [fontsLoaded] = useFonts({
+    Poppins_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={{ alignItems: "center" }}>
-      <Text>Home screen</Text>
+    <View style={styles.root}>
+      <Text style={styles.greet}>Hello Geby</Text>
+
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  root:{
+    alignItems: 'center',
+    padding: 30
+  },
+  greet:{
+    fontFamily: 'Poppins_600SemiBold'
+  }
+})
+
+function class_box(){
+  
+  return(
+    <View>
+
+    </View>
+  )
+}
 export default Home;
+
